e8dd557c0f3308b0f160101ed409c6a8b8a2e4e5
Fiji22-06 <Fiji.FQ@gmail.com>
2026-07-07 18:24:40 +0800

    Delete manajemen_toko directory

--- manajemen_toko/templates/footer.php
+++
@@ -1,7 +0,0 @@
-            </div>
-        </main>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="<?= BASE_URL; ?>assets/js/script.js"></script>
-</body>
-</html>
